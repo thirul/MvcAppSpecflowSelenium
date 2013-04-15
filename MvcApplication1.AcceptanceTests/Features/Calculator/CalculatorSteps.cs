92ec8ae68a72dc4d394f7df6db6c2b7735aa644b
Thiru Sudagoni <2thiru@gmail.com>
2013-04-15 09:56:14 +0100

    Specflow & Selenium tests added

---
+++ MvcApplication1.AcceptanceTests/Features/Calculator/CalculatorSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MvcApplication1.AcceptanceTests.Features.Calculator
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        Calculator _calculator = null;
+        int _result = 0;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            //ScenarioContext.Current.Pending();
+            _calculator.AddNumber(p0);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            //ScenarioContext.Current.Pending();
+            _result = _calculator.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0, _result);
+        }
+    }
+
+    public class Calculator
+    {
+        IList<int> _numbers = new List<int>();
+
+        internal void AddNumber(int p0)
+        {
+            _numbers.Add(p0);
+        }
+
+        internal int Add()
+        {
+            return _numbers.Sum();
+        }
+    }
+}
---
+++ MvcApplication1.SeleniumTests/UnitTest1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace MvcApplication1.SeleniumTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver webDriver = null;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            webDriver = new FirefoxDriver();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            webDriver.Url = "http://gmail.com";
+
+            var emailElement = webDriver.FindElement(By.Id("Email"));
+            var passwordElement = webDriver.FindElement(By.Id("Passwd"));
+
+            emailElement.SendKeys("invalid");
+            passwordElement.SendKeys("invalid");
+
+            var signInElement = webDriver.FindElement(By.Id("signIn"));
+            signInElement.Click();
+
+            var webDriverWait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(15));
+            webDriverWait.Until((d) => 
+            {
+                // var errorMessage = webDriver.FindElement(By.ClassName("errormsg"));
+                // return errorMessage.Text;
+
+                return d.Title.Contains("sudagoni@gmail.com");
+            });
+
+            //var errorMessage1 = webDriver.FindElement(By.ClassName("errormsg"));
+            //Assert.IsTrue(errorMessage1.Text.Contains("incorrect123"));
+
+            // valid case
+            //var userNameElement = webDriver.FindElement(By.Id("gbi4t"));
+            //Assert.AreEqual(userNameElement.Text, "Goud Sudagoni23");
+
+            var userImage = webDriver.FindElement(By.Id("gbi4i"));
+            userImage.Click();
+            var signOutElement = webDriver.FindElement(By.Id("gb_71"));
+            signOutElement.Click();
+
+            var webDriverWaitSignOut = new WebDriverWait(webDriver, TimeSpan.FromSeconds(15));
+            webDriverWaitSignOut.Until((d) =>
+            {
+                // var errorMessage = webDriver.FindElement(By.ClassName("errormsg"));
+                // return errorMessage.Text;
+
+                return d.Title.Contains("Email from Google");
+            });
+
+            Assert.IsTrue(webDriver.Title.Contains("Email from "));
+
+
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            webDriver.Close();
+        }
+    }
+}
